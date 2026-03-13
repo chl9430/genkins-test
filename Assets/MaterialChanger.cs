57e334af6e5b466bdc8e5f06c2b3ade2bd323b9a
JUNG HOO CHOI <chl9430@gmail.com>
2026-03-13 19:12:43 +0900

    Create MaterialChanger.

---
+++ Assets/MaterialChanger.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour
+{
+    [SerializeField] float changePerTime;
+    [SerializeField] List<Material> mats;
+
+    float curTime;
+    int curMatIdx;
+    MeshRenderer renderer;
+
+    private void Update()
+    {
+        curTime += Time.deltaTime;
+
+        if (curTime >= changePerTime)
+        {
+            curTime = 0;
+
+            if (renderer == null)
+                renderer = GetComponent<MeshRenderer>();
+
+            renderer.material = mats[curMatIdx];
+            curMatIdx++;
+
+            if (curMatIdx == mats.Count)
+                curMatIdx = 0;
+        }
+    }
+}
