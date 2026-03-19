f9ef51da6d35f4848ad08e48c9d17ba1b6368442
JUNG HOO CHOI <chl9430@gmail.com>
2026-03-19 21:03:53 +0900

    Installed StarterAsset and set mobile environment.

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,15 +1,21 @@
 using UnityEditor;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 public class BuildScript
 {
     public static void PerformBuild()
     {
-        // 빌드할 씬 목록 (본인 프로젝트의 씬 경로로 수정하세요)
-        string[] scenes = { "Assets/Scenes/SampleScene.unity" };
+        string[] scenes = { "Assets/Scenes/Playground.unity" }; // 실제 씬 경로로 수정하세요
+        string buildPath = "Builds/MyGame.apk"; // 확장자를 .apk로 변경
 
-        // 빌드 옵션 설정 (저장 경로와 타겟)
-        BuildPipeline.BuildPlayer(scenes, "Builds/Windows/TestBuild.exe",
-            BuildTarget.StandaloneWindows64, BuildOptions.None);
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = scenes;
+        buildPlayerOptions.locationPathName = buildPath;
+        buildPlayerOptions.target = BuildTarget.Android; // 안드로이드 타겟 지정
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        // ... 생략 (나머지 로그 출력 부분)
     }
 }
---
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+#endif
+
+namespace StarterAssets
+{
+	public class StarterAssetsInputs : MonoBehaviour
+	{
+		[Header("Character Input Values")]
+		public Vector2 move;
+		public Vector2 look;
+		public bool jump;
+		public bool sprint;
+
+		[Header("Movement Settings")]
+		public bool analogMovement;
+
+		[Header("Mouse Cursor Settings")]
+		public bool cursorLocked = true;
+		public bool cursorInputForLook = true;
+
+        //public Button sprintButton; // 인스펙터에서 스프린트 버튼을 연결하세요
+        //public Color activeColor = Color.green; // 달리기 중일 때 색상
+        //public Color normalColor = Color.white; // 걷기 중일 때 색상
+
+#if ENABLE_INPUT_SYSTEM
+        public void OnMove(InputValue value)
+		{
+			MoveInput(value.Get<Vector2>());
+		}
+
+		public void OnLook(InputValue value)
+		{
+			if(cursorInputForLook)
+			{
+				LookInput(value.Get<Vector2>());
+			}
+		}
+
+		public void OnJump(InputValue value)
+		{
+			JumpInput(value.isPressed);
+		}
+
+		public void OnSprint(InputValue value)
+		{
+			SprintInput(value.isPressed);
+		}
+#endif
+
+
+		public void MoveInput(Vector2 newMoveDirection)
+		{
+			move = newMoveDirection;
+		} 
+
+		public void LookInput(Vector2 newLookDirection)
+		{
+			look = newLookDirection;
+		}
+
+		public void JumpInput(bool newJumpState)
+		{
+			jump = newJumpState;
+		}
+
+		public void SprintInput(bool newSprintState)
+		{
+            sprint = !sprint; // 토글
+        }
+
+		private void OnApplicationFocus(bool hasFocus)
+		{
+			SetCursorState(cursorLocked);
+		}
+
+		private void SetCursorState(bool newState)
+		{
+			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+		}
+	}
+	
+}
