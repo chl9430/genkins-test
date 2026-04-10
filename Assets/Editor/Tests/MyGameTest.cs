577d470b716c3c9477f313d7b32eb14a190dd213
JUNG HOO CHOI <chl9430@gmail.com>
2026-04-10 20:33:30 +0900

    Added test code.

---
+++ Assets/Editor/Tests/MyGameTest.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+using UnityEngine;
+
+// 클래스 이름이 파일명(MyGameTests.cs)과 일치해야 합니다.
+public class MyGameTest
+{
+    [Test] // [UnityTest]가 아닙니다!
+    public void MySimpleTest()
+    {
+        Assert.AreEqual(100, 10 + 20);
+    }
+}
