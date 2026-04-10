163649935729feb8e86956b0ab2035f7933be7f6
JUNG HOO CHOI <chl9430@gmail.com>
2026-04-10 22:16:34 +0900

    test

---
+++ Assets/Editor/MyGameTest.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+
+[TestFixture] // 이 속성을 추가해보세요.
+public class MyGameTest
+{
+    [Test]
+    public void SimplePassTest()
+    {
+        Assert.AreEqual(30, 0); // 0 == 30 실패 케이스
+    }
+}
